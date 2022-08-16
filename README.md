a8ff5d7896518f411625aa8188cd5910c13fe293
yu2x <yu2x@163.com>
2022-08-16 11:11:08 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# sp-supermarquee-vue-example-in9jfc
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/sp-supermarquee-vue-example-in9jfc)
---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <h2>My Vue.js App</h2>
+    <super-marquee content="SuperMarquee says 'Hello' to Vue.js."></super-marquee>
+  </div>
+</template>
+
+<script>
+
+import 'sp-supermarquee/dist/SuperMarqueeWC.js';
+
+export default {
+  name: 'App',
+  components: {}
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+super-marquee {
+  font-family: 'Courier New', Courier, monospace;
+  font-size: larger;
+  font-weight: bold;
+}
+</style>
